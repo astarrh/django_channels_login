244655819e00b185a227f48634662056412fd36b
Jessamyn Smith <geekchick77@gmail.com>
2021-08-18 20:04:30 -0600

    Added home page and login/logout functionality.

--- django_channels_login/urls.py
+++ django_channels_login/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth import urls as auth_urls
+from django.urls import include, path
+
+from myapp import views as myapp_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include(auth_urls)),
+    path('', myapp_views.IndexView.as_view(), name='index'),
 ]
